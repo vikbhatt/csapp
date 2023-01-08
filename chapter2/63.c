230815f44f2b24b8efe5436189eab9a4b7330030
Vikram Bhatt <vikrambhatt1405@gmail.com>
2023-01-09 02:45:40 +0530

    wip

--- chapter2/63.c
+++ chapter2/63.c
@@ -2,5 +2,11 @@
 
 unsigned srl(unsigned x,  int k){
     unsigned xsra = (int) x >> k;
+    int mask = 0 ? (1 << ((sizeof(int) << 3) - k)) - 1 : 0;
+    return xsra | mask;
+}
+
+unsigned sra(int x, int k){
+    int xsrl = (unsigned) x >> k;
     
 }
